34a049f5c90c966ee316f711916fc5eb52214f7e
Steve Melia <steve.j.melia@gmail.com>
2016-09-12 00:01:06 +0100

    More alterations for travis

--- build.sh
+++ build.sh
@@ -14,7 +14,7 @@ dotnet restore
 # Ideally we would use the 'dotnet test' command to test netcoreapp and net451 so restrict for now 
 # but this currently doesn't work due to https://github.com/dotnet/cli/issues/3073 so restrict to netcoreapp
 
-dotnet test ./test/TEST_PROJECT_NAME -c Release -f netcoreapp1.0
+dotnet test ./test/ArrayOfBytes.OAuth.Client.Tests -c Release -f netcoreapp1.0
 
 # Instead, run directly with mono for the full .net version 
 #dotnet build ./test/TEST_PROJECT_NAME -c Release -f net451
@@ -26,4 +26,4 @@ dotnet test ./test/TEST_PROJECT_NAME -c Release -f netcoreapp1.0
 revision=${TRAVIS_JOB_ID:=1}  
 revision=$(printf "%04d" $revision) 
 
-dotnet pack ./src/PROJECT_NAME -c Release -o ./artifacts --version-suffix=$revision  
+dotnet pack ./src/ArrayOfBytes.OAuth.Client -c Release -o ./artifacts --version-suffix=$revision  
